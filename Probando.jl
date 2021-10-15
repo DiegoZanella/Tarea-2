b8ac3c1db3742beaa6fac354e9a012a71503e41e
DiegoEscritorio <diegozanella@ciencias.unam.mx>
2021-10-15 14:57:42 -0500

    Your Message

--- Probando.jl
+++ Probando.jl
@@ -1,35 +1,39 @@
-function PolyModel(n,t,y)
-    #Número de observaciones
-    m = length(t)
+using LinearAlgebra
 
-    #Calculamos la matriz de diseño A. Esta es una matriz de  mxn. La entrada A_{ij} = t_i^j
-    A = zeros(m,n);
-    for i = 1:m
-        for j = 1:n
-            A[i,j] = t[i]^j
-        end
-    end
+function Householder(A)
+    (m,n)=size(A)
 
+    x = A[:,1]
 
-    #Buscamos la factorización de Cholesky de A^T A
-    (L,U) = FacChol(A'*A)
+    e1 = zeros(m)
+    e1[1] = 1.0
 
-    # Encontramos una solución w_0 a la ecuación lineal  Lw = A^Ty mediante la función SolFwd
-    #   (porque L es triangular inferior).
-    w0 = SolFwd(L,A'*y)
+    v = x+sign(A[1,1])*norm(x)*e1
+    v = v/norm(v)
 
-    # Encontramos una solución c_0 a la ecuación lineal L^Tc = w_0 mediante la función SolBwd
-    # (porque L^T es triangular superior).
-    c0 = SolBwd(L',w0)
-
-    #Aquí construimos la función polinomial PolyMod(x) = c0^T * (1,x, ... , x^n)^T
-    function PolyFun(x)
-        y = zeros(n+1)
-        for i = 0:n
-            y[i+1] = x^i
-        end
-        return c'*y
-    end
-
-    return(PolyFun)
+    H = I -2*v*v'
 end
+
+#Esta función toma como argumento una matriz A de mxn. Regresa una tupla (Q,R) donde Q es una matriz ortogonal y
+# R es una matriz triangular superior tales que QR = A.
+function QRHouseholder(A)
+    (m,n) = size(A)
+    Q = Matrix(1.0*I,m,m)
+    R = A
+
+    for i = 1:n
+        HTild = Householder(R[i:m,i:n] )
+
+        H = Matrix(1.0*I,m,m)
+        H[i:m,i:m] = HTild
+    
+        R = H*R
+        Q = H*Q
+    end
+    Q = Q'
+    return((Q,R))
+end
+
+A = [1 2; 3 4; 5 6]
+
+(Q,R) = QRHouseholder(A)
